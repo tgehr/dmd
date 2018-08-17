bd6c80549163209383d8d3927a2252a7a7464b4d
Timon Gehr <timon.gehr@gmx.ch>
2018-08-17 13:25:56 +0200

    Parse tuple expressions.

--- compiler/src/dmd/id.d
+++ compiler/src/dmd/id.d
@@ -452,6 +452,8 @@ immutable Msgtable[] msgtable =
     { "outpw"},
     { "builtinsModuleName", "builtins" },
     { "ctfeWrite", "__ctfeWrite" },
+    { "typecons" }, // TODO: remove and put tuple and Tuple in object.d
+    { "tuple" },
 
     // Traits
     { "isAbstractClass" },
