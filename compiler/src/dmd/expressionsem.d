908e992e368c1268f5e2f8887f3352c0d9457686
Timon Gehr <timon.gehr@gmx.ch>
2023-09-01 17:36:30 +0200

    Semantic analysis for UnpackDeclaration in foreach variable.

--- compiler/src/dmd/expressionsem.d
+++ compiler/src/dmd/expressionsem.d
@@ -17546,7 +17546,7 @@ void lowerNonArrayAggregate(StaticForeach sfe, Scope* sc)
         foreach (params; pparams)
         {
             auto p = sfe.aggrfe ? (*sfe.aggrfe.parameters)[i] : sfe.rangefe.param;
-            params.push(new Parameter(aloc, p.storageClass, p.type, p.ident, null, null, p.unpack));
+            params.push(new Parameter(aloc, p.storageClass, p.type, p.ident, null, null, p.unpack ? p.unpack.syntaxCopy(null) : null));
         }
     }
     Expression[2] res;
