86e1906d067a99548b294af79b56904ad5cac45d
Timon Gehr <timon.gehr@gmx.ch>
2024-09-05 00:48:58 +0200

    Semantic analysis for UnpackDeclaration in function literal parameter.

--- compiler/src/dmd/typesem.d
+++ compiler/src/dmd/typesem.d
@@ -548,6 +548,12 @@ void purityLevel(TypeFunction typeFunction)
         if (!t)
             continue;
 
+        if (fparam.unpack)
+        {
+            fparam.unpack.propagateStorageClasses();
+            fparam.storageClass |= fparam.unpack.storage_class;
+        }
+
         if (fparam.storageClass & (STC.lazy_ | STC.out_))
         {
             typeFunction.purity = PURE.weak;
@@ -2379,6 +2385,11 @@ Type typeSemantic(Type type, Loc loc, Scope* sc)
             for (size_t i = 0; i < dim; i++)
             {
                 Parameter fparam = tf.parameterList[i];
+                if (fparam.unpack)
+                {
+                    fparam.unpack.propagateStorageClasses();
+                    fparam.storageClass |= fparam.unpack.storage_class;
+                }
                 fparam.storageClass |= STC.parameter;
                 mtype.inuse++;
                 fparam.type = fparam.type.typeSemantic(loc, argsc);
