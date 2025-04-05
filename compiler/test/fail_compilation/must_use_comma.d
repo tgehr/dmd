e2aa0c3113bfd83ec172e64b2cce133c835a1401
Nick Treleaven <ntrel002@gmail.com>
2025-04-05 13:21:16 +0100

    Fix fail_compilation/must_use_comma.d
    Ignore local phobos path/line nos.

--- compiler/test/fail_compilation/must_use_comma.d
+++ compiler/test/fail_compilation/must_use_comma.d
@@ -1,7 +1,9 @@
 /+
 TEST_OUTPUT:
 ---
-fail_compilation/must_use_comma.d(16): Error: ignored value of `@mustuse` type `must_use_comma.S`; prepend a `cast(void)` if intentional
+fail_compilation/must_use_comma.d(18): Error: template `tuple` is not callable using argument types `!()(S, void)`
+$p:phobos/std/typecons.d$($n$):        Candidate is: `tuple(Names...)`
+$p:phobos/std/typecons.d$($n$):          - Containing: `tuple(Args...)(Args args)`
 ---
 +/
 import core.attribute;
